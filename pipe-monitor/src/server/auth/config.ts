eb72d668a1634d552a5b5a25cfd75f0d0a452209
youssef-hsn <youssefhsn16@gmail.com>
2025-08-13 20:50:15 +0300

    feat(server): initialize pipe-monitor project use create-t3-app
    - Add PostCSS configuration for Tailwind CSS
    - Add Prettier configuration with Tailwind CSS plugin
    - Include favicon.ico in the public directory
    - Set up NextAuth.js API route for authentication
    - Create global layout component with metadata and font integration
    - Implement home page with links to documentation and first steps
    - Establish environment variable management with validation
    - Configure authentication with Drizzle ORM adapter
    - Set up database connection and schema for users, accounts, sessions, and verification tokens
    - Add global CSS styles with Tailwind CSS
    - Create TypeScript configuration for the project

---
+++ pipe-monitor/src/server/auth/config.ts
@@ -0,0 +1,57 @@
+import { DrizzleAdapter } from "@auth/drizzle-adapter";
+import { type DefaultSession, type NextAuthConfig } from "next-auth";
+
+import { db } from "~/server/db";
+import {
+  accounts,
+  sessions,
+  users,
+  verificationTokens,
+} from "~/server/db/schema";
+
+/**
+ * Module augmentation for `next-auth` types. Allows us to add custom properties to the `session`
+ * object and keep type safety.
+ *
+ * @see https://next-auth.js.org/getting-started/typescript#module-augmentation
+ */
+declare module "next-auth" {
+  interface Session extends DefaultSession {
+    user: {
+      id: string;
+      // ...other properties
+      // role: UserRole;
+    } & DefaultSession["user"];
+  }
+
+  // interface User {
+  //   // ...other properties
+  //   // role: UserRole;
+  // }
+}
+
+/**
+ * Options for NextAuth.js used to configure adapters, providers, callbacks, etc.
+ *
+ * @see https://next-auth.js.org/configuration/options
+ */
+export const authConfig = {
+  providers: [
+    
+  ],
+  adapter: DrizzleAdapter(db, {
+    usersTable: users,
+    accountsTable: accounts,
+    sessionsTable: sessions,
+    verificationTokensTable: verificationTokens,
+  }),
+  callbacks: {
+    session: ({ session, user }) => ({
+      ...session,
+      user: {
+        ...session.user,
+        id: user.id,
+      },
+    }),
+  },
+} satisfies NextAuthConfig;
